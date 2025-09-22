34a78780cf062bd911daffe4e48faf49145010e1
adityashettyyy <aditya.shetty24@vit.edu>
2025-09-22 22:56:29 +0530

    Switch Statements

---
+++ Step 1/ps3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    void whichWeekDay(int day)
+    {
+        switch (day)
+        {
+        case 1:
+            cout << "Monday";
+            break;
+
+        case 2:
+            cout << "Tuesday";
+            break;
+
+        case 3:
+            cout << "Wednesday";
+            break;
+
+        case 4:
+            cout << "Thursday";
+            break;
+
+        case 5:
+            cout << "Friday";
+            break;
+
+        case 6:
+            cout << "Saturday";
+            break;
+
+        case 7:
+            cout << "Sunday";
+            break;
+
+        default:
+            cout << "Invalid";
+        }
+    }
+};
+
+int main()
+{
+    Solution obj;
+    int day;
+
+    cout << "Enter day number(1-7): ";
+    cin >> day;
+
+    obj.whichWeekDay(day);
+
+    return 0;
+}
